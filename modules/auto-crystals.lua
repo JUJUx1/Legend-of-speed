cce7f11dacea47c4c9034aaf412aff7ac5743fdf
JUJUx1 <bs4729926@gmail.com>
2025-10-13 09:26:43 +0545

    Update auto-crystals.lua

--- modules/auto-crystals.lua
+++ modules/auto-crystals.lua
@@ -1,22 +1,20 @@
--- WORKING AUTO-CRYSTALS
 local crystalToggle = false
 
-local function OpenCrystals()
-    while crystalToggle and wait(1) do
-        pcall(function()
-            game:GetService("ReplicatedStorage").rEvents.openCrystalRemote:InvokeServer("openCrystal", "Red Crystal")
-        end)
-    end
-end
-
-return {
-    Toggle = function(state)
-        crystalToggle = state
-        if state then
-            print("💎 AUTO-CRYSTALS ACTIVATED")
-            spawn(OpenCrystals)
-        else
-            print("🛑 AUTO-CRYSTALS STOPPED")
+spawn(function()
+    while wait(2) do
+        if crystalToggle then
+            pcall(function()
+                game:GetService("ReplicatedStorage").rEvents.openCrystalRemote:InvokeServer("openCrystal", "Red Crystal")
+            end)
         end
     end
-}
+end)
+
+return function(state)
+    crystalToggle = state
+    if state then
+        print("💎 AUTO-CRYSTALS ACTIVATED")
+    else
+        print("🛑 AUTO-CRYSTALS STOPPED")
+    end
+end
