4b65bd77be04dbf351ae9b541127500fd625f305
JUJUx1 <bs4729926@gmail.com>
2025-10-13 08:22:31 +0545

    Update auto-rebirth.lua

--- modules/auto-rebirth.lua
+++ modules/auto-rebirth.lua
@@ -1,1 +1,19 @@
+-- AUTO REBIRTH SYSTEM
+local rebirthToggle = false
 
+local function AutoRebirth()
+    while rebirthToggle and wait(0.1) do
+        pcall(function()
+            game:GetService("ReplicatedStorage").rEvents.rebirthEvent:FireServer("rebirthRequest")
+        end)
+    end
+end
+
+return {
+    Toggle = function(state)
+        rebirthToggle = state
+        if state then
+            spawn(AutoRebirth)
+        end
+    end
+}
