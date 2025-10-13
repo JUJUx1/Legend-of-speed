efad26a281c074b129b9917e99f080d7b3298675
JUJUx1 <bs4729926@gmail.com>
2025-10-13 09:25:46 +0545

    Update auto-rebirth.lua

--- modules/auto-rebirth.lua
+++ modules/auto-rebirth.lua
@@ -1,22 +1,20 @@
--- WORKING AUTO-REBIRTH
 local rebirthToggle = false
 
-local function AutoRebirth()
-    while rebirthToggle and wait(0.5) do
-        pcall(function()
-            game:GetService("ReplicatedStorage").rEvents.rebirthEvent:FireServer("rebirthRequest")
-        end)
-    end
-end
-
-return {
-    Toggle = function(state)
-        rebirthToggle = state
-        if state then
-            print("🔄 AUTO-REBIRTH ACTIVATED")
-            spawn(AutoRebirth)
-        else
-            print("🛑 AUTO-REBIRTH STOPPED")
+spawn(function()
+    while wait(1) do
+        if rebirthToggle then
+            pcall(function()
+                game:GetService("ReplicatedStorage").rEvents.rebirthEvent:FireServer("rebirthRequest")
+            end)
         end
     end
-}
+end)
+
+return function(state)
+    rebirthToggle = state
+    if state then
+        print("🔄 AUTO-REBIRTH ACTIVATED")
+    else
+        print("🛑 AUTO-REBIRTH STOPPED")
+    end
+end
